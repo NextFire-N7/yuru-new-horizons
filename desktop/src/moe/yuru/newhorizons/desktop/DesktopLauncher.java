bf6575eff4c57aaeb7bc2ad58854cd87279af2c2
NextFire <git@yuru.moe>
2021-03-22 18:02:51 +0100

    libgdx gradle

---
+++ desktop/src/moe/yuru/newhorizons/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package moe.yuru.newhorizons.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import moe.yuru.newhorizons.YuruNewHorizons;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new YuruNewHorizons(), config);
+	}
+}
