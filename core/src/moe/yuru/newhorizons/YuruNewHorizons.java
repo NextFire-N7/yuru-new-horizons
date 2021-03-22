85a336ad42e89ac6fe3dfb43d11f67aa0e07b5cf
NextFire <git@yuru.moe>
2021-03-22 23:12:29 +0100

    exemple menu ultra basique

--- core/src/moe/yuru/newhorizons/YuruNewHorizons.java
+++ core/src/moe/yuru/newhorizons/YuruNewHorizons.java
@@ -1,33 +1,97 @@
 package moe.yuru.newhorizons;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class YuruNewHorizons extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private Music yuruTheme;
+	private Sound nyanpasu;
+	private Texture yuruBg;
+	private Texture renchon;
+	private Viewport viewport;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+
+	// Tout ce qui se passe au démarrage du jeu
 	@Override
-	public void create () {
+	public void create() {
+		// Charger la BGM
+		yuruTheme = Gdx.audio.newMusic(Gdx.files.internal("yuru_theme.mp3"));
+		yuruTheme.setLooping(true);
+		yuruTheme.play();
+
+		// Charger la hitsound
+		nyanpasu = Gdx.audio.newSound(Gdx.files.internal("nyanpasu.mp3"));
+		nyanpasu.play();
+
+		// Charger les images (-> en VRAM)
+		yuruBg = new Texture(Gdx.files.internal("menu_bg.jpeg"));
+		yuruBg.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		renchon = new Texture(Gdx.files.internal("renchon.png"));
+		renchon.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+
+		// La Camera fait la traduction entre notre code en 1280x720 et la taille réelle de la fenêtre
+		// Le Viewport permet de garder l'aspect ratio quand on resize la fenêtre
+		// le SpriteBatch enverra les instructions au moteur de rendu
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1280, 720);
+		viewport = new FitViewport(1280, 720, camera);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
+	// Etapes effectuées pour chaque rendu d'une frame
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	public void render() {
+		camera.update();	// maj des données de la camera
+		batch.setProjectionMatrix(camera.combined);		// dit en gros au moteur d'utiliser la camera pour la trad code <-> taille réelle
+		// Les instr de rendu entre begin et end seront faites en 1 coup (> rendu efficace, vive les fps)
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(yuruBg, 0, 0, camera.viewportWidth, camera.viewportHeight);
+		batch.draw(renchon, 100, 0, renchon.getWidth() / 2, renchon.getHeight() / 2);
 		batch.end();
+
+		// C'est le listener pour quand on clique sur Renchon ça fait nyanpasu
+		// Ressemble pas mal à ce qu'on fait avec Swing en TP/TD
+		Gdx.input.setInputProcessor(new InputAdapter() {
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+				if (button == Buttons.LEFT) {
+					Rectangle renchonRectangle = new Rectangle(100, 0, renchon.getWidth() / 2, renchon.getHeight() / 2);
+					Vector3 tmp = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+					camera.unproject(tmp);
+					if (renchonRectangle.contains(tmp.x, tmp.y)) {
+						nyanpasu.play();
+					}
+				}
+				return false;
+			}
+		});
 	}
-	
+
+	// C'est pour gérer la "sauvegarde" de l'aspect ratio quand on resize la fenêtre
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void resize(int width, int height) {
+		viewport.update(width, height);
+	}
+
+	// Evidemment on libère les ressources à la fin du programme
+	@Override
+	public void dispose() {
+		yuruTheme.dispose();
+		nyanpasu.dispose();
+		yuruBg.dispose();
+		renchon.dispose();
 	}
 }
--- desktop/src/moe/yuru/newhorizons/desktop/DesktopLauncher.java
+++ desktop/src/moe/yuru/newhorizons/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import moe.yuru.newhorizons.YuruNewHorizons;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Yuru New Horizons";
+      	config.width = 1280;
+      	config.height = 720;
 		new LwjglApplication(new YuruNewHorizons(), config);
 	}
 }
