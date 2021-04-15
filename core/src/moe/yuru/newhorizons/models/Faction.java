55d3d952a7062ed0d372914da058a2ea578b3a54
NextFire <git@yuru.moe>
2021-04-15 22:52:46 +0200

    better enum

--- core/src/moe/yuru/newhorizons/models/Faction.java
+++ core/src/moe/yuru/newhorizons/models/Faction.java
@@ -5,6 +5,17 @@ package moe.yuru.newhorizons.models;
  */
 public enum Faction {
 
-    SCIENCE, CULTURE, INDUSTRY, POLITICS
+    SCIENCE("Science"), CULTURE("Culture"), INDUSTRY("Industry"), POLITICS("Politics");
+
+    private String name;
+
+    private Faction(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 
 }
