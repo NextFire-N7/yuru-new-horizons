e9899bae8dd0acf1f3a99517acf97bbbd8ff07ff
DinoGurnari <dinogurnari@gmail.com>
2021-05-24 12:37:15 +0200

    Update OpponentNormal.java
    Opponent Normal aura des Coins et Ressources aléatoires basées sur celle du joueur, équilibrage à faire

--- core/src/moe/yuru/newhorizons/models/OpponentNormal.java
+++ core/src/moe/yuru/newhorizons/models/OpponentNormal.java
@@ -1,5 +1,8 @@
 package moe.yuru.newhorizons.models;
 
+import java.util.Random;   
+import com.badlogic.gdx.utils.ObjectMap;
+
 /**
  * Normal difficulty opponent for versus games.
  * 
@@ -7,10 +10,47 @@ package moe.yuru.newhorizons.models;
  */
 public class OpponentNormal implements Opponent {
 
+    private Town player;
+
+    private float coins;
+    private ObjectMap<String, Float> resources;
+
     @Override
     public void update(float delta) {
-        // TODO Auto-generated method stub
+        Random rand = new Random();
+
+        // Coins
+        float randCoins = player.getCoins()*0.8f + rand.nextInt((int) (player.getCoins()/5));
+        addCoins(randCoins);
+
+        // Faction resources
+        for (Faction faction : Faction.values()) {
+            float randResource = player.getResources(faction)*0.8f + rand.nextInt((int) (player.getResources(faction)/5));
+            addResources(faction, randResource);
+        }
         
     }
 
+    void setPlayer(Town player) {
+        this.player = player;
+    }
+
+    /**
+     * @param amount to add/remove
+     */
+    public void addCoins(float amount) {
+        if (coins + amount >= 0) {
+            coins += amount;
+        }
+    }
+
+    /**
+     * @param faction a game faction
+     * @param amount  of resources to add in this faction
+     */
+    public void addResources(Faction faction, float amount) {
+        if (resources.get(faction.name()) + amount >= 0) {
+            resources.put(faction.name(), resources.get(faction.name()) + amount);
+        }
+    }
 }
