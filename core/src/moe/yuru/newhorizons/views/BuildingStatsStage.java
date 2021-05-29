4b3b4f23055e63e1a38b9f0f55b80339c19f4855
DinoGurnari <dinogurnari@gmail.com>
2021-05-29 12:58:34 +0200

    Added VisWindow for Stats

--- core/src/moe/yuru/newhorizons/views/BuildingStatsStage.java
+++ core/src/moe/yuru/newhorizons/views/BuildingStatsStage.java
@@ -1,12 +1,12 @@
 package moe.yuru.newhorizons.views;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.kotcrab.vis.ui.widget.VisLabel;
 import com.kotcrab.vis.ui.widget.VisTable;
 import com.kotcrab.vis.ui.widget.VisTextButton;
+import com.kotcrab.vis.ui.widget.VisWindow;
 
 import moe.yuru.newhorizons.YuruNewHorizons;
 import moe.yuru.newhorizons.models.Building;
@@ -37,9 +37,16 @@ public class BuildingStatsStage extends Stage {
         // this.game = game;
         this.stats = building.getStats(level);
 
+        VisWindow statsWindow = new VisWindow(building.getFunction());
+        addActor(statsWindow);
+
         // A full height table just at the right
         VisTable menuTable = new VisTable();
-        addActor(menuTable);
+        // addActor(menuTable);
+        statsWindow.add(menuTable);
+        statsWindow.setSize(512, 720);
+        statsWindow.setPosition(768, 0);
+
         menuTable.setSize(512, 720);
         menuTable.setPosition(768, 0);
         menuTable.defaults().grow().pad(10);
@@ -53,47 +60,33 @@ public class BuildingStatsStage extends Stage {
 
         // Name and function labels
         VisLabel name = new VisLabel(building.getFirstName() + " " + building.getLastName());
-        name.setColor(Color.BLACK);
         statsTable.add(name);
-        statsTable.row();
-
-        VisLabel function = new VisLabel(building.getFunction());
-        function.setColor(Color.BLACK);
-        statsTable.add(function);
         statsTable.row();
 
         // Level label
         VisLabel lvl = new VisLabel("Level :");
-        lvl.setColor(Color.BLACK);
         statsTable.add(lvl);
         VisLabel lvltxt = new VisLabel(String.valueOf(level));
-        lvltxt.setColor(Color.BLACK);
         statsTable.add(lvltxt);
         statsTable.row();
 
         // Coins balance
         VisLabel coins = new VisLabel("Coins per Seconds :");
-        coins.setColor(Color.BLACK);
         VisLabel coinstxt = new VisLabel(String.valueOf(this.stats.getCoinsPerSecond()));
-        coinstxt.setColor(Color.BLACK);
         statsTable.add(coins);
         statsTable.add(coinstxt);
         statsTable.row();
 
         // Resource label
         VisLabel resource = new VisLabel(this.building.getFaction().toString() + " per Seconds :");
-        resource.setColor(Color.BLACK);
         VisLabel resourcetxt = new VisLabel(String.valueOf(this.stats.getResourcesPerSecond()));
-        resourcetxt.setColor(Color.BLACK);
         statsTable.add(resource);
         statsTable.add(resourcetxt);
         statsTable.row();
 
         // Houses label
         VisLabel homes = new VisLabel("Number of Homes :");
-        homes.setColor(Color.BLACK);
         VisLabel homestxt = new VisLabel(String.valueOf(this.stats.getHomes()));
-        homestxt.setColor(Color.BLACK);
         statsTable.add(homes);
         statsTable.add(homestxt);
 
@@ -110,18 +103,14 @@ public class BuildingStatsStage extends Stage {
             BuildingStats leveledUpStats = this.building.getStats(level + 1);
             // Coins
             VisLabel coinsCostLabel = new VisLabel("Coins to level up :");
-            coinsCostLabel.setColor(Color.BLACK);
             VisLabel coinstxtCostLabel = new VisLabel(String.valueOf(Math.abs(leveledUpStats.getCoinCost())));
-            coinstxtCostLabel.setColor(Color.BLACK);
             costsTable.add(coinsCostLabel);
             costsTable.add(coinstxtCostLabel);
             costsTable.row();
 
             // Resource
             VisLabel resourceCostLabel = new VisLabel(this.building.getFaction().toString() + " to level up :");
-            resourceCostLabel.setColor(Color.BLACK);
             VisLabel resourcetxtCostLabel = new VisLabel(String.valueOf(Math.abs(leveledUpStats.getResourcesCost())));
-            resourcetxtCostLabel.setColor(Color.BLACK);
             costsTable.add(resourceCostLabel);
             costsTable.add(resourcetxtCostLabel);
 
